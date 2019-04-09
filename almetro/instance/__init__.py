8e976d9a88337d9d341d844dc826eb1aaf0df4ff
Arnour Sabino <arnour.sabino@gmail.com>
2019-04-09 18:22:12 -0300

    Create a number sequence provider

--- almetro/instance/__init__.py
+++ almetro/instance/__init__.py
@@ -1,8 +1,25 @@
+import random
+
 class Instance:
-    pass
+
+    def get(self):
+        pass
 
 
 class NumberSequence(Instance):
 
-    def values(self):
-        return ()
+    def __init__(self, size=10, digits=0):
+        """Provides a number sequence instance creator.
+
+        :param int size: number of elements in the instance. Default is 10.
+        :param int digits: number of decimal digits in each instance's element. Default is 0.
+        """
+        self.value = tuple(round(random.random() * size, digits) for i in range(0, size))
+
+    def get(self):
+        """Provides an instance as the form of a number sequence for a simulation.
+
+        :returns: a number sequence. For example: ((0.1, 0.3), (0.4, 0.5))
+        :rtype: tuple(tuple(float))
+        """
+        return self.value
--- almetro/tests/test_number_sequence.py
+++ almetro/tests/test_number_sequence.py
@@ -5,6 +5,11 @@ NumberSequence test.
 from hamcrest import (
     assert_that,
     instance_of,
+    has_length,
+    has_items,
+    matches_regexp,
+    is_not,
+    equal_to,
 )
 
 from almetro.instance import NumberSequence
@@ -12,4 +17,20 @@ from almetro.instance import NumberSequence
 
 def test_should_return_immutable_sequence():
     sequence = NumberSequence()
-    assert_that(sequence.values(), instance_of(tuple))
+    assert_that(sequence.get(), instance_of(tuple))
+
+
+def test_should_return_a_size_3_sequence():
+    sequence = NumberSequence(size=3)
+    assert_that(sequence.get(), has_length(3))
+
+
+def test_should_return_a_randomic_sequence():
+    sequence = NumberSequence(size=3)
+    assert_that(sequence.get()[0], is_not(equal_to(sequence.get()[1])))
+    assert_that(sequence.get()[0], is_not(equal_to(sequence.get()[2])))
+
+
+def test_should_return_rounded_2_digits_sequence():
+    sequence = NumberSequence(size=3, digits=2)
+    assert_that(tuple(map(str, sequence.get())), has_items(matches_regexp("^[0-9]\\.[0-9]{2}$")))
